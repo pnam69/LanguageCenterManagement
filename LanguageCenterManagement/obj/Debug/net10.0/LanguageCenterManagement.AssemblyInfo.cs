9e4a63981f197c817e2d4d56732e9c317d135e4f
pnam69 <67112436+pnam69@users.noreply.github.com>
2026-09-23 22:57:49 +0700

    stack ts

--- LanguageCenterManagement/obj/Debug/net10.0/LanguageCenterManagement.AssemblyInfo.cs
+++ LanguageCenterManagement/obj/Debug/net10.0/LanguageCenterManagement.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LanguageCenterManagement")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b71cccbe9899727c8fe300712fca9c388b66678c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+73d1e9d0578b243f6f36adc73654d5fc4bdbe51e")]
 [assembly: System.Reflection.AssemblyProductAttribute("LanguageCenterManagement")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LanguageCenterManagement")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
